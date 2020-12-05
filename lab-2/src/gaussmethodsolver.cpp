55f6d00df9bf6e02f5fae53c9b45f49778f63704
Mikhail-Zoreev <43145054+Mikhail-Zoreev@users.noreply.github.com>
2020-12-05 18:47:07 +0300

    Update gaussmethodsolver.cpp

--- lab-2/src/gaussmethodsolver.cpp
+++ lab-2/src/gaussmethodsolver.cpp
@@ -4,15 +4,33 @@ Column GaussMethodSolver::solve(const Matrix& A, const Column& b, const Column&,
 {
     Matrix A_copy = A;
     Column b_copy = b;
-    int i, j, k, size = static_cast<int>(b_copy.size());
-    for (j = 0; j < size; j++)
+    size_t size = static_cast<int>(b_copy.size());
+    for (size_t j = 0; j < size; j++)
     {
         double alfa = 0;
-        //TODO: Line swap
-        for (i = j + 1; i < size; i++)
+        if (A_copy(j, j) == 0)
+        {
+            for (size_t k = j + 1; k < A_copy.size(); k += 1)
+            {
+                if (A_copy(j, k) != 0)
+                {
+                    for (size_t i = j; i < A_copy.size(); i++)
+                    {
+                        std::swap(A_copy(i, j), A_copy(i, k));
+                    }
+                    std::swap(b_copy[j], b_copy[k]);
+                    break;
+                }
+            }
+            if (A_copy(j, j) == 0)
+            {
+                break;
+            }
+        }
+        for (size_t i = j + 1; i < size; i++)
         {
             alfa = A_copy(j, i) / A_copy(j, j);
-            for (k = j; k < size; k++)
+            for (size_t k = j; k < size; k++)
             {
                 A_copy(k, i) -= alfa * A_copy(k, j);
             }
@@ -21,10 +39,10 @@ Column GaussMethodSolver::solve(const Matrix& A, const Column& b, const Column&,
     }
     Column result(size);
     result[size - 1] = b_copy[size - 1] / A_copy(size - 1, size - 1);
-    for (i = size - 2; i >= 0; i--)
+    for (int64_t i = size - 2; i >= 0; i--)
     {
         double sum = 0;
-        for (j = i + 1; j < size; j++)
+        for (size_t j = i + 1; j < size; j++)
         {
             sum += A_copy(j, i) * result[j];
         }
